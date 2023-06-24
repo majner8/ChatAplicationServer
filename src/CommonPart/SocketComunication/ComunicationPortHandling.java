022b07c3180a6de2bead5b504a95c9d838bb10b4
anton <anton@LenovoPCPrace.local.tld>
2023-06-24 03:15:16 +0200

    24.6

--- src/CommonPart/SocketComunication/ComunicationPortHandling.java
+++ src/CommonPart/SocketComunication/ComunicationPortHandling.java
@@ -12,6 +12,7 @@ import java.net.SocketTimeoutException;
 import java.time.LocalDateTime;
 import java.util.Random;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
 
 import CommonPart.SocketComunication.SocketComunication.SocketComunicationException;
 import CommonPart.ThreadManagement.ThreadPoolingManagement;
@@ -19,6 +20,7 @@ import Main.Main;
 
 public final class ComunicationPortHandling {
 	
+	private static final String VerifyConnection="\\|&qsr\\";
     private static Random random = new Random();
     private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
@@ -163,8 +165,11 @@ public final class ComunicationPortHandling {
 			this.read=read;
 			this.connectionAutorized=connectionAutorized;
 			this.start();
+			//start sending verify connection
 
 		}
+		
+
 		private byte SocketTimeOutTryAmount=0;
 		private static byte MaximumSocketTimeOutTryAmout=3;
 		private boolean connectionAutorized = false;
@@ -172,6 +177,7 @@ public final class ComunicationPortHandling {
 		private static byte MaximumSecuryTry=3;
 		private byte LengSecurityTry=1;
 		private int maximumCharacterOfOneMessage=8000;
+		
 		@Override
 		public void run() {
 			while(!Main.isServerStopped()&&!TemporaryClosed&&!UnConnected) {
@@ -193,6 +199,7 @@ public final class ComunicationPortHandling {
 							return;
 					    }
 					    
+					    
 					    //chech if end by endMessageCharacter
 					    if(sb.length()>=ComunicationPortHandling.EndMessageCharacter.length()) {
 						    String lastMessage=sb.substring(sb.length()-ComunicationPortHandling.EndMessageCharacter.length());
@@ -204,6 +211,12 @@ public final class ComunicationPortHandling {
 					}
 					if(c==-1) {
 				    	System.out.println("closeB");
+
+				    	System.out.println(socket.isClosed());
+				    	System.out.println(socket.isInputShutdown());
+				    	System.out.println(socket.isConnected());
+				    	System.out.println(socket.isOutputShutdown());
+
 
 						CloseTemporary();
 						return;
@@ -243,8 +256,10 @@ public final class ComunicationPortHandling {
 					continue;
 				}
 
-				final String curentMessage = sb.toString().substring(0,sb.length()-ComunicationPortHandling.EndMessageCharacter.length());
+				 String mes = sb.toString().substring(0,sb.length()-ComunicationPortHandling.EndMessageCharacter.length());
 				
+				
+				final String curentMessage=mes.replaceAll(ComunicationPortHandling.VerifyConnection, "");
 				sb.setLength(0);
 				if(!this.connectionAutorized) {
 					if (!curentMessage.equals(AutorizationKey)) {
@@ -293,9 +308,23 @@ public final class ComunicationPortHandling {
 			this.writer=write;
 			
 			super.start();
-
+			this.SendVerifyMess();
 		}
 
+		
+		private void SendVerifyMess() {
+			ThreadPoolingManagement.thread.schedule(()->{
+				if(UnConnected||TemporaryClosed) {
+					
+					return;
+				}
+				System.out.println("I am sending verification");
+				writeMessage(ComunicationPortHandling.VerifyConnection,true);
+				this.SendVerifyMess();
+			}, 30, TimeUnit.SECONDS);
+
+		}
+		
 		private void close() {
 			try {
 				this.writer.close();
